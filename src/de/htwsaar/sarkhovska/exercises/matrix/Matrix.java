b9ea2a21e2a47e6f4aee036bb975a4993e845a98
Oleksiy Sarkhovskyy <o.sarkhovskyy@gmail.com>
2024-05-21 13:20:28 +0200

    2v

---
+++ src/de/htwsaar/sarkhovska/exercises/matrix/Matrix.java
@@ -0,0 +1,62 @@
+package de.htwsaar.sarkhovska.exercises.matrix;
+
+public class Matrix<T extends Number>  {
+    private T[][] matrix;
+    private T zero;
+
+
+    public Matrix(int rows, int cols, T zero) {
+        matrix = (T[][])  new Object[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = zero;
+            }
+        }
+    }
+
+    public  int getRowNumber() {
+        return matrix.length;
+    }
+
+    public  int getColNumber() {
+        return matrix[0].length;
+    }
+
+    public  void set(int row, int col, T value) {
+        matrix[row][col] = value;
+    }
+
+    public T get(int row, int col) {
+        return matrix[row][col];
+    }
+
+    public <U extends Number> Matrix<Double> add(Matrix<U> other) {
+        if (other == null) {
+            throw new IllegalArgumentException("Other matrix cannot be null");
+        }
+        if (other.getRowNumber() != getRowNumber() || other.getColNumber() != getColNumber()) {
+            throw new IllegalArgumentException("Other matrix has not the same size");
+        }
+
+        Matrix<Double> result = new Matrix<Double>(getRowNumber(),getColNumber(),0.0);
+
+        for (int i = 0; i < getRowNumber(); i++) {
+            for (int j = 0; j < getColNumber(); j++) {
+                Double d = matrix[i][j].doubleValue() + other.matrix[i][j].doubleValue();
+                result.set(i,j,d);
+            }
+        }
+        return result;
+    }
+
+    public <U extends Number> Matrix<Double> multiplyByScalar(U value) {
+        Matrix<Double> result = new Matrix<Double>(getRowNumber(),getColNumber(),0.0);
+        for (int i = 0; i < getRowNumber(); i++) {
+            for (int j = 0; j < getColNumber(); j++) {
+                result.set(i,j,matrix[i][j].doubleValue() * value.doubleValue());
+            }
+        }
+        return result;
+    }
+
+}
