00e73cf3fad88c9a2f82ea59d3b922003610e014
Oleksiy Sarkhovskyy <o.sarkhovskyy@gmail.com>
2024-05-21 08:59:21 +0200

    2v

--- src/de/htwsaar/esch/Codeopolis/DomainModel/LinkedList.java
+++ src/de/htwsaar/esch/Codeopolis/DomainModel/LinkedList.java
@@ -1,39 +1,37 @@
 package de.htwsaar.esch.Codeopolis.DomainModel;
 
-public class LinkedList <E> {
+public class LinkedList <T> {
 
-    protected static class Node<E> {
-        E element;
-        Node<E> next;
+    private  class Node<T> {
+        T element;
+        Node<T> next;
 
-        Node(E element, Node<E> next) {
+        Node(T element, Node<T> next) {
             this.element = element;
             this.next = next;
         }
 
-        Node(E element) {
-            this.element = element;
-            this.next = null;
+        Node(T element) {
+            this(element, null);
         }
     }
 
-    Node<E> head;
+    Node<T> head;
 
     public LinkedList() {
-        this.head = null;
     }
 
-    private Node<E> getLast() {
-        Node<E> result = head;
+    private Node<T> getLast() {
+        Node<T> result = head;
         while (result.next != null) {
             result = result.next;
         }
         return result;
     }
 
-    public void addLast(E element) {
-        Node<E> last = getLast();
-        Node<E> newLast = new Node<>(element);
+    public void addLast(T element) {
+        Node<T> last = getLast();
+        Node<T> newLast = new Node<>(element);
         if (last!=null) {
             last.next = newLast;
         }
@@ -42,8 +40,8 @@ public class LinkedList <E> {
         }
     }
 
-    public E removeFirst() {
-        Node<E> result = head;
+    public T removeFirst() {
+        Node<T> result = head;
         if (result!= null) {
             head = result.next;
             return result.element;
@@ -57,7 +55,7 @@ public class LinkedList <E> {
 
     public int size() {
         int count = 0;
-        Node<E> node = head;
+        Node<T> node = head;
         while (node != null) {
             ++count;
             node = node.next;
@@ -68,4 +66,40 @@ public class LinkedList <E> {
     public  void clear() {
         head = null;
     }
+
+    public T get(int index) {
+        Node<T> node = head;
+        while (index-- != 0 && node != null) {
+            node = node.next;
+        }
+        return node!=null ? node.element: null;
+    }
+
+    public T set(int index, T element) {
+        Node<T> node = head;
+        while (index-- != 0 && node != null) {
+            node = node.next;
+        }
+        if (node!=null) {
+            T prev = node.element;
+            node.element = element;
+            return prev;
+        }
+        return null;
+    }
+
+    public T remove(int index) {
+        Node<T> node = head;
+        Node<T> prev = null;
+        while (index-- != 0 && node != null) {
+            prev = node;
+            node = node.next;
+        }
+        if (prev==null || node==null) {
+            return null;
+        }
+        prev.next = null;
+        return node.element;
+    }
+
 }
