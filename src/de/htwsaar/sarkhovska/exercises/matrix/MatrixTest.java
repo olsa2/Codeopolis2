b9ae7d369b266f2df442267ed6c5a10dc744a22b
Oleksiy Sarkhovskyy <o.sarkhovskyy@gmail.com>
2024-05-21 14:54:35 +0200

    2v

--- src/de/htwsaar/sarkhovska/exercises/matrix/MatrixTest.java
+++ src/de/htwsaar/sarkhovska/exercises/matrix/MatrixTest.java
@@ -8,6 +8,8 @@ public class MatrixTest {
             mi.set(i, 1, i+1);
             mi.set(i, 2, i+1);
         }
+        System.out.println("Integer matrix");
+        System.out.println(mi);
 
         Matrix<Double> md = new Matrix<>(3,3,0.0);
         for (int i = 0; i < 3; i++) {
@@ -15,9 +17,15 @@ public class MatrixTest {
             md.set(i, 1, 1.0* (i+1));
             md.set(i, 2, 1.0* (i+1));
         }
+        System.out.println("Double matrix");
+        System.out.println(md);
+
         Matrix<Double> addition = mi.add(md);
+        System.out.println("Addition");
         System.out.println(addition);
-        Matrix<Double> multiply = mi.multiplyByScalar(1.1f);
+
+        Matrix<Double> multiply = mi.multiplyByScalar(2f);
+        System.out.println("Multiply by scalar " + 2f);
         System.out.println(multiply);
     }
 }
